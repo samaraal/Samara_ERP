44bde055b00dbe3c09ce45ca4dd4851616a8e240
samaraal <samarahealthcare2026@gmail.com>
2026-09-06 02:13:46 +0530

    Add files via upload

--- bootstrap-error.js
+++ bootstrap-error.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
 
-  const VERSION = '2.10.13';
+  const VERSION = '2.10.16';
   let rendering = false;
 
   const escapeHtml = value => String(value ?? '')
--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const APP_VERSION = '2.10.13';
-const CACHE = 'samara-erp-2.10.13-pwa-recovery';
+const APP_VERSION = '2.10.16';
+const CACHE = 'samara-erp-2.10.16-pwa-recovery';
 const SHELL = [
   './',
   './index.html',
