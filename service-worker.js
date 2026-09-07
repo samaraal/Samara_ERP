c3bc12d7e1706d08c8ae7b57b36ded4f568446b9
samaraal <samarahealthcare2026@gmail.com>
2026-09-07 19:38:50 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const APP_VERSION = '2.10.44';
-const CACHE = 'samara-erp-2.10.44-pwa-recovery';
+const APP_VERSION = '2.10.45';
+const CACHE = 'samara-erp-2.10.45-pwa-recovery';
 const SHELL = [
   './',
   './index.html',
