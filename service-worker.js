41896bbb874c1db9326fff69c65144b13ff1dc61
samaraal <samarahealthcare2026@gmail.com>
2026-09-16 21:18:20 +0530

    Share the same printable statement PDF (2.13.15)

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const APP_VERSION = '2.13.14';
-const CACHE = 'samara-erp-2.13.14-food-vendor';
+const APP_VERSION = '2.13.15';
+const CACHE = 'samara-erp-2.13.15-food-vendor';
 const SHELL = [
   './',
   './index.html',
