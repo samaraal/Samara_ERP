b18ae4fd2e15b7080abade23fb38d84f3875142e
samaraal <samarahealthcare2026@gmail.com>
2026-09-16 17:19:24 +0530

    Add meal period totals and focused food actions

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const APP_VERSION = '2.13.03';
-const CACHE = 'samara-erp-2.13.03-food-vendor';
+const APP_VERSION = '2.13.04';
+const CACHE = 'samara-erp-2.13.04-food-vendor';
 const SHELL = [
   './',
   './index.html',
