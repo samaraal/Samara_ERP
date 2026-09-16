73a58e4fe7a3a465c6f70dd996f14c1adb66f842
samaraal <samarahealthcare2026@gmail.com>
2026-09-16 20:58:11 +0530

    Compact History table with tap details (2.13.11)

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const APP_VERSION = '2.13.10';
-const CACHE = 'samara-erp-2.13.10-food-vendor';
+const APP_VERSION = '2.13.11';
+const CACHE = 'samara-erp-2.13.11-food-vendor';
 const SHELL = [
   './',
   './index.html',
