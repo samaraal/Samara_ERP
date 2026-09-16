0eeded219f2315a2b6acffabd02fa10b89707d5b
samaraal <samarahealthcare2026@gmail.com>
2026-09-16 20:05:00 +0530

    Add refresh button to Food Vendor Management

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const APP_VERSION = '2.13.04';
-const CACHE = 'samara-erp-2.13.04-food-vendor';
+const APP_VERSION = '2.13.05';
+const CACHE = 'samara-erp-2.13.05-food-vendor';
 const SHELL = [
   './',
   './index.html',
