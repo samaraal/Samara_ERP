4476035e6da9f555f815109401e142da685e6969
samaraal <samarahealthcare2026@gmail.com>
2026-09-22 10:56:25 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const APP_VERSION = '2.14.00';
-const CACHE = 'samara-erp-2.14.00-food-vendor';
+const APP_VERSION = '2.14.01';
+const CACHE = 'samara-erp-2.14.01-food-vendor';
 const SHELL = [
   './',
   './index.html',
