974e32d22c3dc78d4da24ef952f7c48f3bf32bbd
rajaiahboomi-crypto <rajaiahboomi@gmail.com>
2026-09-24 14:00:42 +0530

    v2.14.38: version bump

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const APP_VERSION = '2.14.37';
-const CACHE = 'samara-erp-2.14.37-mobile-home-signout';
+const APP_VERSION = '2.14.38';
+const CACHE = 'samara-erp-2.14.38-workflow-popup-fix';
 const SHELL = [
   './',
   './index.html',
